a049b495f16808e22a62e162a9db7b196ab9eded
pwinning1991 <ftcpjw@yahoo.com>
2024-08-09 17:49:41 -0400

    first commit

---
+++ api.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	listenAddr string
+}
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJson(w, http.StatusBadRequest, ApiError{err.Error()})
+		}
+	}
+}
+
+func NewApiServer(listenAddr string) *APIServer {
+	return &APIServer{listenAddr: listenAddr}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	log.Println("JSON Api server Listening on " + s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == http.MethodGet {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == http.MethodPost {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == http.MethodDelete {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("Unsupported method: %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pwinning1991/gobank
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1
